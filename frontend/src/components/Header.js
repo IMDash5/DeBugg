cb8e62beeb8e88c056db2bec6e4254e9d3745f2a
CHUVISS <aleksandrchuvilov@MacBook-Pro-Aleksandr-2.local>
2025-03-13 15:13:20 +0300

    Добавил машрутизацию

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
 export default function Header({ setIsSidebarOpen }) {
@@ -8,19 +9,23 @@ export default function Header({ setIsSidebarOpen }) {
   return (
     <header className="header">
       <h1 className="title">Debugg™</h1>
-
       <div 
         className="profile-container" 
         onMouseEnter={() => setIsMenuOpen(true)} 
         onMouseLeave={() => setIsMenuOpen(false)}
       >
+
         <span className="user-name">{userName}</span>
-        <div className="profile-icon">
+        <div 
+          className="profile-icon" 
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
           <img src="/images/account_logo.png" className="icon" alt="профиль"/>
           <div className={`dropdown-menu ${isMenuOpen ? "open" : ""}`}>
-            <a href="#" className="menu-item">Профиль</a>
-            <a href="#" className="menu-item">Оплата</a>
-            <a href="#" className="menu-item">Выход</a>
+            <Link to="/profile" className="menu-item">Профиль</Link>
+            <Link to="/home" className="menu-item">Загрузить резюме</Link>
+            <Link to="/payment" className="menu-item">Оплата</Link>
+            <button className="menu-item logout-button" onClick={() => alert("Выход")}>Выход</button>
           </div>
         </div>
       </div>
