503ab702674e10a2c2b0e04eba1b5906fbd7a8f7
Danil Kaurov <danilkaurov05556@gmail.com>
2025-03-18 00:21:30 +0300

    Добавлен парсер PDF файла

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,8 +1,10 @@
 from typing import Union
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, File, UploadFile, status
 from sqlalchemy.orm import Session
 from models.database import SessionLocal
 from models.crud import create_user, get_user
+from models.parser_pdf import pdf_parser
+
 
 app = FastAPI()
 
@@ -34,3 +36,30 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+
+# Эндпоинт для загрузки одного файла
+@app.post("/upload_file/")
+async def upload_file(uploaded_file: UploadFile):
+    filename = f"1_{uploaded_file.filename}"
+    with open(filename, "wb") as f:
+        f.write(await uploaded_file.read())
+    
+    parsed_text = pdf_parser(filename)
+    return {"parsed_text": parsed_text}
+
+# Эндпоинт для загрузки нескольких файлов
+@app.post("/multiple_upload_files/")
+async def upload_files(uploaded_files: list[UploadFile]):
+    results = []
+    for uploaded_file in uploaded_files:
+        filename = f"1_{uploaded_file.filename}"
+        with open(filename, "wb") as f:
+            f.write(await uploaded_file.read())
+        
+        parsed_text = pdf_parser(filename)
+        results.append({
+            "filename": filename,
+            "parsed_text": parsed_text
+        })
+    return {"files": results}
