513ea88b329fd8019af42d5bd8a3595e7f04c194
CHUVISS <chuvilova2005@gmail.com>
2025-03-22 22:31:55 +0300

    Добавил бета шаблон профиля, добавил пункты в меню, исправил ошибки не вывода страниц

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@ import Home from "./pages/Home.js";
 import Payment from "./pages/Payment.js";
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
+import Account from "./pages/Account.js"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Account />} />
         <Route path="/home" element={<Home />} />
         <Route path="/payment" element={<Payment />} />
       </Routes>
---
+++ frontend/src/pages/Account.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../styles/Account.css';
+
+export default function Account() {
+  return (
+    <div className="account-container">
+      <div className="account-sidebar">
+        <img
+          src="/images/account_logo.png"
+          alt="avatar"
+          className="account-avatar"
+        />
+        <h2 className="account-name">Иван Иванов</h2>
+        <p className="account-role">Common account</p>
+        {/* Можно указывать тип подписки */}
+      </div>
+
+      <div className="account-info">
+        <h3 className="account-section-title">Information</h3>
+        <div className="account-info-row">
+          <div>
+            <p className="account-label">Email</p>
+            <p>info@example.com</p>
+          </div>
+          <div>
+            <p className="account-label">Phone</p>
+            <p>123 456 789</p>
+          </div>
+        </div>
+
+
+        <div className="account-info-row">
+          <div>
+            <p className="account-label">Recent</p>
+            <p>Lorem ipsum</p>
+          </div>
+          <div>
+            <p className="account-label">Most Viewed</p>
+            <p>Dolor sit amet</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
